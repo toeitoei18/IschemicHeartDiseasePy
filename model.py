8f3068096fa096b56786720c49b08a12c24e0545
toeitoei18 <hs4toei2544@gmail.com>
2025-03-10 07:38:50 +0700

    WorkFromHome 3

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-# mydata = pd.read_csv(r'C:\Users\SKH-PC02\Downloads\IschemicHeartDiseasePy\IschemicHeartDisease.csv')
+#mydata = pd.read_csv(r'C:\Users\Administrator\IschemicHeartDiseasePy\IschemicHeartDisease.csv')
 mydata = pd.read_csv(r'C:\Users\Toei\Desktop\IschemicHeartDiseasePy\IschemicHeartDisease.csv')
 
 x = mydata[['ChestTightness','ChestPain','Smart','HeartPalpitations','Squeamish','Faint','Gasp','Tired','Choking','EpigastricCongestion']]
@@ -31,7 +31,7 @@ load_model = pickle.load(open(filename,'rb'))
 feature_names = ['ChestTightness','ChestPain','Smart','HeartPalpitations','Squeamish','Faint','Gasp','Tired','Choking','EpigastricCongestion']
 
 # Convert input to a DataFrame
-input_data = pd.DataFrame([[1, 1, 1, 0, 0, 0, 0, 0, 0, 0]], columns=feature_names)
+input_data = pd.DataFrame([[1, 1, 1, 1, 1, 0, 0, 0, 0, 0]], columns=feature_names)
 
 # Make prediction
 print(load_model.predict(input_data))
